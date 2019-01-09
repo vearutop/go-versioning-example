16a91a4d0692685ae35ef6799644b43a0149edd4
Viacheslav Poturaev <vearutop@gmail.com>
2019-01-09 14:46:51 +0100

    Meaningless change

--- version.go
+++ version.go
@@ -8,18 +8,14 @@ import (
 )
 
 func init() {
-	// delaying init to allow build info setting in another init
-	// see https://github.com/golang/go/issues/29628
-	// see https://golang.org/src/cmd/go/internal/modload/build.go#L234
-	go func() {
-		info, available := debug.ReadBuildInfo()
-		if !available {
-			println("build info not available")
-			return
-		}
-		fmt.Printf("Build info: %+v\n", info)
-		if info.Main.Version != "" {
-			version = info.Main.Version
-		}
-	}()
+	info, available := debug.ReadBuildInfo()
+	if !available {
+		println("build info not available")
+		return
+	}
+	fmt.Printf("Build info: %+v\n", info)
+	if info.Main.Version != "" {
+		version = info.Main.Version
+	}
+	//
 }
