eeb20d936f03bbc73f2a885900c57c494395420f
Viacheslav Poturaev <vearutop@gmail.com>
2019-01-09 12:57:25 +0100

    Workaround init precedence issue

--- version.go
+++ version.go
@@ -8,13 +8,18 @@ import (
 )
 
 func init() {
-	info, available := debug.ReadBuildInfo()
-	if !available {
-		println("build info not available")
-		return
-	}
-	fmt.Printf("Build info: %+v\n", info)
-	if info.Main.Version != "" {
-		version = info.Main.Version
-	}
+	// delaying init to allow build info setting in another init
+	// see https://github.com/golang/go/issues/29628
+	// see https://golang.org/src/cmd/go/internal/modload/build.go#L234
+	go func() {
+		info, available := debug.ReadBuildInfo()
+		if !available {
+			println("build info not available")
+			return
+		}
+		fmt.Printf("Build info: %+v\n", info)
+		if info.Main.Version != "" {
+			version = info.Main.Version
+		}
+	}()
 }
